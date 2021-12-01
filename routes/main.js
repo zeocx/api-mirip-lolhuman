a4a843508e4fe7381c53d0f38a11e400ad1a25f7
zeeone <82263175+zeeoneofc@users.noreply.github.com>
2021-12-02 02:47:42 +0800

    update main.js

--- routes/main.js
+++ routes/main.js
@@ -10,41 +10,54 @@ router.get('/', (req, res) => {
 router.get('/docs', (req, res) => {
 	res.sendFile(__path + '/docs/dashboard.html')
 })
-
-router.get('/docs/photooxy', (req, res) => {
-	res.sendFile(__path + '/api/photooxy.html')
+router.get('/docs/asupan', (req, res) => {
+	res.sendFile(__path + '/api/asupan.html')
 })
-
-router.get('/docs/media', (req, res) => {
-	res.sendFile(__path + '/api/media.html')
-})
-
 router.get('/docs/anime', (req, res) => {
 	res.sendFile(__path + '/api/anime.html')
 })
 
-router.get('/docs/game', (req, res) => {
-	res.sendFile(__path + '/api/game.html')
-})
-
-router.get('/docs/other', (req, res) => {
-	res.sendFile(__path + '/api/other.html')
+router.get('/docs/cecan', (req, res) => {
+	res.sendFile(__path + '/api/cecan.html')
 })
 
 router.get('/docs/downloader', (req, res) => {
 	res.sendFile(__path + '/api/downloader.html')
 })
 
-router.get('/docs/info', (req, res) => {
-	res.sendFile(__path + '/api/info.html')
+router.get('/docs/filmapik', (req, res) => {
+	res.sendFile(__path + '/api/filmapik.html')
 })
 
-router.get('/docs/islami', (req, res) => {
- 	res.sendFile(__path + '/api/islami.html')
+router.get('/docs/game', (req, res) => {
+	res.sendFile(__path + '/api/game.html')
 })
 
-router.get('/docs/fun', (req, res) => {
-	res.sendFile(__path + '/api/fun.html')
+router.get('/docs/islam', (req, res) => {
+	res.sendFile(__path + '/api/islam.html')
+})
+
+router.get('/docs/lk21', (req, res) => {
+	res.sendFile(__path + '/api/lk21.html')
+})
+
+router.get('/docs/news', (req, res) => {
+ 	res.sendFile(__path + '/api/news.html')
+})
+
+router.get('/docs/nsfw', (req, res) => {
+	res.sendFile(__path + '/api/nsfw.html')
+})
+router.get('/docs/other', (req, res) => {
+	res.sendFile(__path + '/api/other.html')
+})
+
+router.get('/docs/photooxy', (req, res) => {
+ 	res.sendFile(__path + '/api/photooxy.html')
+})
+
+router.get('/docs/search', (req, res) => {
+	res.sendFile(__path + '/api/search.html')
 })
 
 router.get('/config', (req, res) => {
